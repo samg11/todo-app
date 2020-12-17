8e3be16cd607975f95c8713aaf520bb7e2369dc1
Sam Girshovich <sam.girshovich@gmail.com>
2020-12-17 13:34:20 -0500

    basic error handling

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app.config["SECRET_KEY"] = secrets.readSecret('secretkey')
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    if request.method == "GET":
+    try:
         username = session.get("username")
         password = session.get("password")
         if database.authenticate(username, password):
@@ -21,60 +21,80 @@ def index():
             session["username"] = ""
             session["password"] = ""
             return redirect(url_for('signup'))
+    
+    except:
+        flash('an unknown error occured')
+        return redirect(url_for('login'))
+
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        if database.username_taken(username):
-            if database.authenticate(username, password):
-                session["username"] = username
-                session["password"] = password
-                return redirect(url_for('index'))
+    try:
+        if request.method == "POST":
+            username = request.form["username"]
+            password = request.form["password"]
+            if database.username_taken(username):
+                if database.authenticate(username, password):
+                    session["username"] = username
+                    session["password"] = password
+                    return redirect(url_for('index'))
+                else:
+                    flash("Incorrect username or password")
+                    return redirect(url_for('login'))
+
             else:
-                flash("Incorrect username or password")
-                return redirect(url_for('login'))
+                flash(f'{username} does not exist, please create an account')
+                return redirect(url_for('signup'))
 
-        else:
-            flash(f'{username} does not exist, please create an account')
-            return redirect(url_for('signup'))
-
-    return render_template('login.html')
+        return render_template('login.html')
+    
+    except:
+        flash('an unknown error occured')
+        return render_template('error.html')
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
-    if request.method == "GET":
-        return render_template("signup.html")
+    try:
+        if request.method == "GET":
+            return render_template("signup.html")
 
-    elif request.method == "POST":
-        if request.form.get("ageCheck"):
-            if request.form["password1"] != request.form["password2"]:
-                flash('passwords do not match')
-                return redirect(url_for('signup'))
+        elif request.method == "POST":
+            if request.form.get("ageCheck"):
+                if request.form["password1"] != request.form["password2"]:
+                    flash('passwords do not match')
+                    return redirect(url_for('signup'))
 
-            elif database.username_taken(request.form["username"]):
-                flash('username has been taken')
-                return redirect(url_for('signup'))
+                elif database.username_taken(request.form["username"]):
+                    flash('username has been taken')
+                    return redirect(url_for('signup'))
+
+                else:
+                    session["username"] = request.form["username"]
+                    session["password"] = request.form["password1"]
+                    database.add_user(request.form["username"], request.form["password1"])
+                    flash(database.account_creation_status(request.form["username"], request.form["password1"]))
+                    return redirect(url_for('index'))
 
             else:
-                session["username"] = request.form["username"]
-                session["password"] = request.form["password1"]
-                database.add_user(request.form["username"], request.form["password1"])
-                flash(database.account_creation_status(request.form["username"], request.form["password1"]))
-                return redirect(url_for('index'))
-
-        else:
-            flash('You must be at least 13 years old to sign up')
-            return redirect(url_for('signup'))
+                flash('You must be at least 13 years old to sign up')
+                return redirect(url_for('signup'))
+    except:
+        return render_template('error.html')
 
 @app.route('/addtodo')
 def add_todo():
-    todo = request.args["todo"]
-    if todo.replace('\n', 'a') != todo:
-        flash('Todos must be one line')
-    else:
-        database.submit_todo(todo, session["username"])
+    try:
+        todo = request.args["todo"]
+
+        if todo.replace('\n', 'a') != todo:
+            flash('Todos must be one line')
+
+        else:
+            database.submit_todo(todo, session["username"])
+    
+    except:
+        flash('An error occured submiting your todo')
+
     return redirect(url_for('index'))
 
 @app.route('/remove_todo', methods=["GET", "POST"])
@@ -84,9 +104,23 @@ def removetodo():
 
 @app.route('/logout')
 def logout():
-    session["username"] = ""
-    session["password"] = ""
-    return redirect(url_for('login'))
+    try:
+        session["username"] = ""
+        session["password"] = ""
+        return redirect(url_for('login'))
+
+    except:
+        flash('There was an error logging you out')
+        return redirect(url_for('index'))
+
+@app.route('/errortest')
+def errortest():
+    try:
+        raise Exception('an error has occured')
+
+    except:
+        flash('testing error page')
+        return render_template('error.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
